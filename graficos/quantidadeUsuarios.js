606554e720239dd6263e0ed1227949295867cd75
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-11-11 14:27:51 -0300

    f

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -31,7 +31,7 @@ async function quantidadeUsuarios() {
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
+  Plotly.newPlot(grafico, data, layout)
   
   }
   
